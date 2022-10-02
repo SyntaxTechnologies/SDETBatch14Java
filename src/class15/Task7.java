b67f9a858061effe6b00b1fc698b993345e90e08
Asghar Nazir <asgharkrk@gmail.com>
2022-10-02 12:43:46 -0400

    All Tasks

--- src/class15/Task7.java
+++ src/class15/Task7.java
@@ -14,7 +14,7 @@ anything else - F
         char grade;
         if(score>90){
            grade='A';
-        }else if(score>80){
+        }else if( score>80){
             grade='B';
         }else if(score>70){
             grade='C';
