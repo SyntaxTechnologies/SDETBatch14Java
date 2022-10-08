8f7495babfc70ce86d2c8597710a347d6bb3e53a
Asghar Nazir <asgharkrk@gmail.com>
2022-10-08 12:51:25 -0400

    Class 18

--- src/class18/Task3.java
+++ src/class18/Task3.java
@@ -6,4 +6,28 @@ public class Task3 {
     class: 1 - inside same class; 2 - from outside the class; 3 - from different class inside different package
     and observe result.
      */
+
+    private Task3(){
+        System.out.println("private");
+    }
+
+     Task3(String name){
+        System.out.println("Default");
+    }
+
+   protected Task3(int age){
+        System.out.println("protected");
+    }
+    public Task3(boolean isTrue){
+        System.out.println("public");
+    }
+
+    public static void main(String[] args) {
+        new Task3();
+        new Task3("Thank you Lubna");
+        new Task3(10);
+        new Task3(true);
+    }
+
+
 }
