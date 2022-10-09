520db5f9f2eec311dff74d508334f68d3006949b
Asghar Nazir <asgharkrk@gmail.com>
2022-10-09 13:05:52 -0400

    Class 19

--- src/class19/Bird.java
+++ src/class19/Bird.java
@@ -5,7 +5,7 @@ public class Bird {
     String color;
     int age;
     double weight;
-    Bird(String name,String color,int age,double weight){
+ public Bird(String name,String color,int age,double weight){
         this.name=name;
         this.color=color;
         this.age=age;
