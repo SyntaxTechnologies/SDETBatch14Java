d19a882e0aaf99147954187bf10ec1227af8702c
Asghar Nazir <asgharkrk@gmail.com>
2022-10-09 12:27:10 -0400

    Class 19

--- src/class19/Crow.java
+++ src/class19/Crow.java
@@ -1,30 +1,20 @@
 package class19;
 
 class Parrot extends Bird{
-
     Parrot(String name,String color,int age,double weight){
-      //  super(name, color, age, weight);
-     /*   this.name=name;
-        this.age=age;
-        this.weight=weight;
-        this.color=color;*/
+        super(name, color, age, weight);
     }
 }
 class Crow extends Bird{
-    Crow(String name,String color,int age,double weight){
-        /*this.name=name;
-        this.age=age;
-        this.weight=weight;
-        this.color=color;*/
-       // super(name, color, age, weight);
+    boolean isClever;
+    Crow(String name,String color,int age,double weight,boolean isClever){
+       super(name, color, age, weight);
+       this.isClever=isClever;
     }
 }
 class Sparrow extends Bird{
     Sparrow(String name,String color,int age,double weight){
-        /*this.name=name;
-        this.age=age;
-        this.weight=weight;
-        this.color=color;*/
-       // super(name, color, age, weight);
+        super(name, color, age, weight);
+
     }
 }
