3f915179041e45daa17af0154b5727ccd9a08b0a
Asghar Nazir <asgharkrk@gmail.com>
2022-10-11 19:30:50 -0400

    Class 20

---
+++ src/class20/Task3.java
@@ -0,0 +1,11 @@
+package class20;
+
+public class Task3 {
+    /*
+    Write a Java program called Teacher.
+    Identify features and behaviour of that Class. Create 3 subclasses MathTeacher,
+     ChemistryTeacher and PianoTeacher that would have it their own features and behaviour.
+     Test all 4 classes
+     */
+
+}
