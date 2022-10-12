99ab26f333dc8b9f6d0098f6e0f3314d5119bb39
Asghar Nazir <asgharkrk@gmail.com>
2022-10-12 19:59:51 -0400

    Class 20

---
+++ src/class21/Task2.java
@@ -0,0 +1,20 @@
+package class21;
+
+public class Task2 {
+
+    /*
+Create a class named 'Programming'.
+While creating an object of the class,
+if nothing is passed to it,
+then the message "I love programming languages" should be printed.
+If some String is passed to it,
+then in place of "programming languages" the value variable should be printed.
+Example, if we pass "Java", then "I love Java" should be printed.
+ */
+
+
+}
+
+class Programming{
+
+}
