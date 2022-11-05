c79a68df6f63e0ec4003506f25f1ce7725180ab5
Asghar Nazir <asgharkrk@gmail.com>
2022-11-05 12:16:24 -0400

    Class 26

--- src/class26/Task1.java
+++ src/class26/Task1.java
@@ -1,5 +1,7 @@
 package class26;
 
+import java.util.ArrayList;
+
 public class Task1 {
     public static void main(String[] args) {
 
@@ -7,7 +9,28 @@ public class Task1 {
  /* Create an arrayList of words.
        Remove every word that ends with “e”.
      */
+        //Break till 11:55
 
+
+        ArrayList<String> words=new ArrayList();
+        words.add("Java");
+        words.add("India");
+        words.add("Palestine");
+        words.add("USA");
+        words.add("France");
+        words.add("Pakistan");
+        words.add("Ukraine");
+
+        var iterator=words.iterator();
+
+        while (iterator.hasNext()){
+            var item=iterator.next();
+            if(item.endsWith("e")){
+                iterator.remove();
+            }
+        }
+
+        System.out.println(words);
 
     }
 }
