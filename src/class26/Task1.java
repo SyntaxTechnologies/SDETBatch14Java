e06aae161f38f4918cb8a64648cc7b143ca4bfde
Asghar Nazir <asgharkrk@gmail.com>
2022-11-05 11:18:17 -0400

    Class 26

---
+++ src/class26/Task1.java
@@ -0,0 +1,13 @@
+package class26;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+
+ /* Create an arrayList of words.
+       Remove every word that ends with “e”.
+     */
+
+
+    }
+}
