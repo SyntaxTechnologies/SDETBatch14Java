11e4392c81dcc92b1466e9dfbb4788686d65d495
Asghar Nazir <asgharkrk@gmail.com>
2022-11-12 12:09:12 -0500

    Class 28

--- src/class28/MapDemo4.java
+++ src/class28/MapDemo4.java
@@ -2,7 +2,7 @@ package class28;
 
 import java.util.HashMap;
 
-public class MapsDemo2 {
+public class MapDemo4 {
     public static void main(String[] args) {
         HashMap<String,Double> items=new HashMap<>();
         items.put("Iphone 14",1000.0);
@@ -11,10 +11,10 @@ public class MapsDemo2 {
         items.put("Keyboard",100.0);
         items.put("Flower",1.0);
 
+        for (Double value: items.values()
+             ) {
+            System.out.println(value);
+        }
 
-
-
-        items.keySet().removeIf(name-> name.contains("e"));
-        System.out.println(items);
     }
 }
--- src/class28/MapDemo5.java
+++ src/class28/MapDemo5.java
@@ -2,7 +2,10 @@ package class28;
 
 import java.util.HashMap;
 
-public class MapsDemo2 {
+public class MapDemo5 {
+
+   // var lastname; does not work with instance variables as java can't figure out the data type as there are no values
+    // assigned
     public static void main(String[] args) {
         HashMap<String,Double> items=new HashMap<>();
         items.put("Iphone 14",1000.0);
@@ -11,10 +14,12 @@ public class MapsDemo2 {
         items.put("Keyboard",100.0);
         items.put("Flower",1.0);
 
+        //Set<Map.Entry<String,Double>> entrySet =items.entrySet();
+        var entrySet=items.entrySet();
+     /*   var dog=new Dog();
+        var number=10.2;*/
 
 
 
-        items.keySet().removeIf(name-> name.contains("e"));
-        System.out.println(items);
     }
 }
