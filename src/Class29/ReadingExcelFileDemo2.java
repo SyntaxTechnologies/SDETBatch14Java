dd52128d02607e9978b902bda949ecaed779ef9f
Asghar Nazir <asgharkrk@gmail.com>
2022-11-13 12:47:52 -0500

    Class 29

--- src/Class29/ReadingExcelFileDemo2.java
+++ src/Class29/ReadingExcelFileDemo2.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class ReadingExcelFileDemo2 {
 
     public static void main(String[] args) throws IOException {
+
      var excelData=   ExcelReader.read("/Users/apple/IdeaProjects/SDETBatch14Java/Data/Test.xlsx");
         System.out.println(excelData);
 
