7c9ed9f6cfec80f6d1add4017536a6c13f5eefa9
Asghar Nazir <asgharkrk@gmail.com>
2022-11-19 13:00:41 -0500

    Last Java Class

--- src/class30/TypesOfExcep.java
+++ src/class30/TypesOfExcep.java
@@ -5,8 +5,8 @@ import utils.ExcelReader;
 import java.io.IOException;
 
 public class TypesOfExcep {
-    public static void main(String[] args) {
-        String name=null;
+    public static void main(String[] args)  {
+     /*   String name=null;
         if(name!=null){
             System.out.println(name.length());
         }
@@ -22,11 +22,18 @@ public class TypesOfExcep {
         if(index<arr.length){
             System.out.println(arr[index]);
         }
+*/
 
 
-
+        try {
             System.out.println(ExcelReader.read("/Users/apple/IdeaProjects/SDETBatch14Java/Data/Test.xlsx"));
+        } catch (IOException e) {
+            System.out.println("Josh can you please stop deleting my files");
+        }catch (NullPointerException e){
 
+        }
+
+        System.out.println("important line of code");
 
     }
 }
