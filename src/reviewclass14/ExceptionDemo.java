758e7e4f2b2c0156c97149ed0215f77a289bddab
Asghar Nazir <asgharkrk@gmail.com>
2022-11-21 20:49:08 -0500

    Last Java Class

--- src/reviewclass14/ExceptionDemo.java
+++ src/reviewclass14/ExceptionDemo.java
@@ -6,5 +6,7 @@ public class ExceptionDemo {
     public static void main(String[] args) {
         ConfigReader.read("skjdbfjsdhbf");
         System.out.println("important line of code");
+
+
     }
 }
