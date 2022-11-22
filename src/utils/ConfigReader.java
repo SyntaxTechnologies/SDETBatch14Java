69c95c3312efaf7313b1b34cb7eb3ea7e31a0263
Asghar Nazir <asgharkrk@gmail.com>
2022-11-21 20:39:26 -0500

    Last Java Class

--- src/utils/ConfigReader.java
+++ src/utils/ConfigReader.java
@@ -10,8 +10,9 @@ public class ConfigReader {
     public static Properties read (String path)  {
 
        var properties = new Properties(); // that special software which helps us read data from that file
+       FileInputStream fileInputStream=null;
         try {
-            var fileInputStream = new FileInputStream(path); //it helps us Navigate to the file
+            fileInputStream = new FileInputStream(path); //it helps us Navigate to the file
             properties.load(fileInputStream); //loads all the data from the file inside(Memory)
         }catch ( FileNotFoundException e){
            // System.out.println("Please check your file path"); normal humans no technical background
@@ -19,7 +20,15 @@ public class ConfigReader {
         }catch (IOException e){
             //System.out.println("Please check your hardrive");
         }
-
+        finally {
+            try {
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return properties;
 
 
